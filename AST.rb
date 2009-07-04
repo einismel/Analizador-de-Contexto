036c29a6a5dfdabb3dae1feb2cd65a9c9ca7b895
Elias Matheus <e3@localhost.localdomain>
2009-07-04 14:28:46 -0430

    AST con aritmetica

--- AST.rb
+++ AST.rb
@@ -7,6 +7,7 @@
 #  Contiena la clase abstracto de arbol del compilador de yisiel
 
 class AST
+  attr_accessor :num, :sign
 	def initialize(num, sign)
 		@num = num
 		@sign = sign
@@ -17,22 +18,74 @@ class AST
   end 
 end
 
-class ArbolBinario < AST
-	def initialize(term1, op, term2)
+class ASTBinario < AST
+  attr_accessor :term1,:term2
+	def initialize(term1, term2)
 		@term1 = term1
-		@op = op
 		@term2 = term2
 	end
 end
 
-class ArbolUnario < AST
-	def initialize(exp)
-		@exp = exp
-	end
-end 
-
-class ASTMath < Arbol Binario
+class ASTMath < ASTBinario
   def check(symtable, symtableG)
   end
 end
 
+class ASTSuma < ASTMath
+  def run(symtable, symtableG)
+    term1 = @term1.run(symtable,symtableG)  
+    term2 = @term2.run(symtable,symtableG)  
+    return term1 + term2
+  end
+end
+
+class ASTResta < ASTMath
+  def run(symtable, symtableG) 
+    term1 = @term1.run(symtable,symtableG)  
+    term2 = @term2.run(symtable,symtableG)  
+    return term1 - term2
+  end
+end
+
+class ASTMult < ASTMath
+  def run(symtable, symtableG) 
+    term1 = @term1.run(symtable,symtableG)  
+    term2 = @term2.run(symtable,symtableG)  
+    return term1 * term2
+  end
+end
+
+class ASTDiv < ASTMath
+  def run(symtable, symtableG)
+    term1 = @term1.run(symtable,symtableG)  
+    term2 = @term2.run(symtable,symtableG)  
+    return term1 / term2 if term2 != 0
+  end
+end
+
+class ASTRes < ASTMath
+  def run(symtable, symtableG) 
+    term1 = @term1.run(symtable,symtableG)  
+    term2 = @term2.run(symtable,symtableG)  
+    return term1 * term2
+  end
+end
+
+class ASTResUnario < ASTBinario
+  def run(symtable, symtableG) 
+    term1 = @term1.run(symtable,symtableG)  
+    term2 = @term2.run(symtable,symtableG)  
+    return term1*-1 
+  end
+end
+
+class ASTUnario < AST
+  attr_accessor :term1
+	def initialize(exp)
+		@term1 = exp
+	end
+
+  def run(symtable, symtableG) 
+    return term1.value
+  end
+end 
